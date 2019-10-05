8c51b31a67efa0091e83dd09b890ae6a1ca5202d
Zachary Schulz <zjschulz@gmail.com>
2019-10-05 00:00:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name,grade)
+        @roster[grade] = []
+        @roster.each do |g, hash|
+        hash << name
+        hash.sort
+        end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+end
